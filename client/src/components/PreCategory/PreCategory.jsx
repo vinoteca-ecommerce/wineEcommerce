c7a0bfe6e29d6c1a3af6bd6fe6117757a0863ad0
RomaGarcia <roarga10@gmail.com>
2022-05-24 18:33:29 -0300

    Component PreCategory demo 1 card

--- client/src/components/PreCategory/PreCategory.jsx
+++ client/src/components/PreCategory/PreCategory.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import style from './PreCategory.module.css';
 
 export const PreCategory = () => {
   return (
-    <div>PreCategory</div>
+    <div className={style.container}>
+        <div className={style.card}>
+            <div className={style.cardImage}>IMAGEN</div>
+            <div className={style.cardDescription}>
+                <p className={style.textTitle}> Card Title</p>
+                {/*<p className={style.textBody}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>*/}
+            </div>
+        </div>
+    </div>
   )
 }
