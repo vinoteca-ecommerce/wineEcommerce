533d5d35a7c4ef91119dbae58ea0eaf3690b5e43
RomaGarcia <roarga10@gmail.com>
2022-05-25 12:50:50 -0300

    PreCategory2. enlarge cards,add category sparkling and combine nav with preCategory

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { NavBar } from './components/NavBar/NavBar';
+import { PreCategory } from './components/PreCategory/PreCategory';
 
 function App() {
   return (
     <div className="App">
       {/*PONER SUS RUTAS/COMPONENTES PARA HACER PRUEBAS jejeje*/}
+      <NavBar/>
       <Routes>
-        <Route path='/' element={<NavBar/>}/>
+        <Route path='/' element={<PreCategory/>}/>
       </Routes>
     </div>
   );
