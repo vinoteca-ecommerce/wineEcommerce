a8cda4555fe1b9f73d45f4cff89ab4c7564a464c
Martin Nicolas Russo <russomartin2@gmail.com>
2022-05-25 17:29:35 -0300

    NavbarFinish

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -7,11 +7,14 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
-const settings = ['Home', 'Products', 'Login/Sing in', 'About Us', 'Contact'];
+const pages = ['Productos', 'Contacto', 'Acerca de'];
+const settings = ['Perfil', 'Cerrar sesión'];
 
 export const NavBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -33,7 +36,7 @@ export const NavBar = () => {
   };
 
   return (
-    <AppBar position="static" style={{ background: 'transparent', boxShadow: 'none'}}>
+    <AppBar position="static" sx={{background:'white'}}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Typography
@@ -42,47 +45,92 @@ export const NavBar = () => {
             component="a"
             href="/"
             sx={{
-                mr: 2,
-                display: { xs: 'none', md: 'flex' },
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'black',
-                textDecoration: 'none',
+              mr: 5,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'gray',
+              boxShadow:'16px 12px maroon',
+              textDecoration: 'none',
             }}
-            >
-            Henry's wines
+          >
+           VINOTECA
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
               aria-controls="menu-appbar"
               aria-haspopup="true"
               onClick={handleOpenNavMenu}
-              color="inherit"
             >
               <MenuIcon />
             </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
           </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'gray',
+              textDecoration: 'none',
+            }}
+          >
+            VINOTECA
+          </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'black', display: 'block'}}
+              >
+                {page}
+              </Button>
+            ))}
           </Box>
-          <Box>
-          <IconButton>
-          <AddShoppingCartIcon />
+            <Box>
+          <IconButton sx={{mr:'20px'}}>
+          <AddShoppingCartIcon fontSize='large' />
           </IconButton>
           </Box>
           <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open Menu">
-              <IconButton onClick={handleOpenUserMenu} 
-                sx={{ mr: 2, color: 'black'}}
-                anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-              >
-                <MenuIcon/>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
@@ -90,14 +138,14 @@ export const NavBar = () => {
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
+                vertical: 'top',
+                horizontal: 'right',
+              }}
               keepMounted
               transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
+                vertical: 'top',
+                horizontal: 'right',
+              }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
