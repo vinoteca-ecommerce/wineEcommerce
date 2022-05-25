8c53307ec5f62fa49188dfb3aa51d83e39c7ab1e
RomaGarcia <roarga10@gmail.com>
2022-05-25 16:26:22 -0300

    home funcionando falta footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,14 @@ import './App.css';
 
 import { Routes, Route } from "react-router-dom";
 import { NavBar } from './components/NavBar/NavBar';
-import { PreCategory } from './components/PreCategory/PreCategory';
-import { Landing } from './components/Landing/Landing';
-
+import { Home } from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      {/*PONER SUS RUTAS/COMPONENTES PARA HACER PRUEBAS jejeje*/}
       <NavBar/>
       <Routes>
-        <Route path='/' element={<PreCategory/>}/>
-        <Route path='/' element={<Landing/>}/>
+        <Route path='/' element={<Home/>}/>
       </Routes>
     </div>
   );
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { PreCategory } from '../PreCategory/PreCategory';
+import { Landing }  from '../Landing/Landing';
+
+export const Home = () => {
+  return (
+    <>
+        <Landing/>
+        <PreCategory/>
+    </>
+  )
+}
