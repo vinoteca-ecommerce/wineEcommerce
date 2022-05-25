04480f2e068dc0395bd6965e90946bcf401ddaf2
Maira Pajaro <maira_pajaro@hotmail.com>
2022-05-25 17:05:06 +0200

    landing

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,15 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import { NavBar } from './components/NavBar/NavBar';
+import { Landing } from './components/Landing/Landing';
 
 function App() {
   return (
     <div className="App">
       {/*PONER SUS RUTAS/COMPONENTES PARA HACER PRUEBAS jejeje*/}
       <Routes>
-        <Route path='/' element={<NavBar/>}/>
+        <Route path='/' element={<Landing/>}/>
+       
       </Routes>
     </div>
   );
