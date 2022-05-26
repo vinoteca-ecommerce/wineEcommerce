68850fbb0b2a8c5191668be442d159e6d499e592
Maira Pajaro <maira_pajaro@hotmail.com>
2022-05-26 16:21:25 +0200

    landing

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import style from '../Landing/Landing.module.css'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 export const Landing = () =>{
     return (
        <div className={style.container}>
          <div className={style.imgBox}>
                    <h1> Discover your favorite wine</h1> 
-                  <p>EXPLORE</p>         
+                  <div><KeyboardArrowDownIcon/> </div>         
              </div>
        </div>      
-        
     )
 }
 
