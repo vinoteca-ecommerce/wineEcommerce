939cd17e71498903b1cbde5825732e364bd59ad1
RomaGarcia <roarga10@gmail.com>
2022-05-26 17:27:11 -0300

    products pagination

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,28 +1,47 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getWines } from '../../redux/actions/actions';
 import { CardProduct } from '../CardProduct/CardProduct';
 import { Container } from '@mui/system';
+import { ProductsPagination } from '../Pagination/ProductsPagination';
 
 export const Products = () => {
     const dispatch = useDispatch();
     const wines = useSelector((state) => state.wines);
 
+    const [page,setPage] = useState(1);
+    const [totalPage, setTotalPage] = useState(1);
+    
+
     useEffect(()=>{
-        dispatch(getWines())
-    },[dispatch])
+        if(page === 1) dispatch(getWines(0));
+        else dispatch(getWines((page*10)-10))
+    },[dispatch,page])
+
+    useEffect(()=>{
+        let num = Math.ceil((wines?.total / 10))
+        if(typeof num === 'number') setTotalPage(num)
+    },[wines.total])
 
     //console.log(wines.products[0]._id)
+    /*if(typeof wines?.total === 'number'){
+        let num = Math.ceil((wines.total / 10))
+        //setTotalPage(num)
+        if(typeof num === 'number') console.log(num)
+    }*/
+    
+
   return (
     <div>
         <Container  maxWidth="xl" sx={{display:'flex', justifyContent:'center', alignItems:'center', flexWrap:'wrap'}}>
-            {wines && wines.products?.length && wines?.products?.map(wine =>(
+            {wines && wines.products?.length !== 0 && wines?.products?.map(wine =>(
                 <div  key={wine._id}>
                     <CardProduct id={wine._id} name={wine.name} producer={wine.producer} year={wine.year} description={wine.description} price={wine.price}
                                 img={wine.img} category={wine.category.name} stock={wine.stock} country={wine.country}  strain={wine.strain}/>
                 </div>
             ))}
         </Container>
+        <ProductsPagination setPage={setPage} page={page} totalPage={totalPage}/>
     </div>
   )
 }
--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 export const GET_WINES = 'GET_WINES';
 
-export const getWines = () => {
+export const getWines = (num) => {
     return async function (dispatch) {
-      return axios.get("http://localhost:8000/products/")
+      return axios.get("http://localhost:8000/products?start="+num)
       .then(response => {
         dispatch({ type: GET_WINES, payload: response.data });
       })
