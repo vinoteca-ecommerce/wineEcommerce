3b11329cf4a2beb90310a5153fbfccf7d4732081
Camilo Arriagada Vallejos <95378920+c4miloarriagada@users.noreply.github.com>
2022-05-31 12:25:34 -0400

    postProduct

--- client/src/components/AdminDashboard/AdminDashboard.jsx
+++ client/src/components/AdminDashboard/AdminDashboard.jsx
@@ -6,45 +6,160 @@ import { getCategories, postWine } from "../../redux/actions/actions";
 
 export const AdminDashboard = () => {
   const dispatch = useDispatch();
+  const category = useSelector((state) => state.categories);
   const [input, setInput] = useState({
-    name: "",
-    year: "",
-    description: "",
-    img: "",
-    category: [],
+    name:'',
+    year:'',
+    description:'',
+    img:'',
+    category: '',
     price: "",
     country: "",
+    strain: '',
+    producer:''
   });
-  let category = useSelector((state) => state.categories);
 
-useEffect(()=>{
-    dispatch(getCategories())
-},[dispatch])
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch]);
+
+  function handleSubmit(e){
+    e.preventDefault()
+    console.log(input)
+    dispatch(postWine(input))
+    setInput({
+      name:'',
+      year:'',
+      description:'',
+      img:'',
+      category: '',
+      price: "",
+      country: "",
+      strain: '',
+      producer:''
+  
+    })
+  }
+
+  function handleSelect(e){
+    setInput({
+      ...input,
+      category: e.target.value
+    });
+  }
+
+
+  function handleOnChange(e) {
+    setInput((state) => {
+      const newState = {
+        ...state,
+        [e.target.name]: e.target.value,
+      };
+      return newState;
+    });
+  }
 
   return (
     <div>
-      <form>
+      <form onSubmit={e=>handleSubmit(e)}>
         <ul>
           <li>
-            <label>NOMBRE:</label>
-            <input type="text" placeholder="NOMBRE" />
+            <label>Nombre:  </label>
+            <input 
+              type="text" 
+              placeholder="Nombre"
+              value={input.name}
+              name='name'
+              autoComplete="off"
+              onChange={handleOnChange} />
           </li>
           <li>
-            <label>AÑO:</label>
-            <input type="text" placeholder="AÑO" />
+            <label>Año:  </label>
+            <input 
+              type="text" 
+              placeholder="Año"
+              value={input.year}
+              name='year' 
+              autoComplete="off"
+              onChange={handleOnChange}
+              />
           </li>
           <li>
-            <label>DESCRIPCION:</label>
-            <input type="text" placeholder="DESCRIPCION" />
+            <label>Cepa:  </label>
+            <input 
+              type="text" 
+              placeholder="Cepa"
+              value={input.strain}
+              name='strain'
+              autoComplete="off"
+              onChange={handleOnChange}
+              />
           </li>
           <li>
-            <label>CATEGORIA:</label>
-            <select type="text" placeholder="CATEGORIA" />
+            <label>Pais:  </label>
+            <input 
+              type="text" 
+              placeholder="Pais"
+              value={input.country}
+              name='country' 
+              autoComplete="off"
+              onChange={handleOnChange}
+              />
           </li>
           <li>
-            <label>PRECIO:</label>
-            <select type="text" placeholder="PRECIO" />
+            <label>Productor:  </label>
+            <input 
+              type="text" 
+              placeholder="Productor"
+              value={input.producer}
+              name='producer' 
+              autoComplete="off"
+              onChange={handleOnChange}
+              />
+           </li>
+           <li>
+            <label>Link Imagen:  </label>
+            <input 
+              type="text" 
+              placeholder="Link Imagen"
+              value={input.img}
+              name='img' 
+              autoComplete="off"
+              onChange={handleOnChange}
+              />
+           </li>
+          <li>
+
+            <label>PRECIO: $ </label>
+            <input 
+              type="text" 
+              placeholder="Precio"
+              value={input.price}
+              name='price' 
+              autoComplete="off"
+              onChange={handleOnChange}
+              />
+           </li>
+          
+              <label > Categoria: </label>
+              <select onChange={e=>handleSelect(e)} >
+                {category.result?.map((e) => (
+                  <option value={e._id} key={e._id}> {e.name} </option>
+                ))}
+              </select>      
+            <li>
+            <label>DESCRIPCION:  </label>
+            <textarea 
+              type="text" 
+              placeholder="Descripcion"
+              value={input.description}
+              name='description'
+              autoComplete="off"
+              onChange={handleOnChange}
+              />
           </li>
+      
+              <button type="submit"> Submit </button>
         </ul>
       </form>
     </div>
