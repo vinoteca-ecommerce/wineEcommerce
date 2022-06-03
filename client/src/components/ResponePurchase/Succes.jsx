c66801e179478292409d13dfbcf3dcf045ee52e5
Gonzalo <gonzalo.hernandez1@hotmail.com>
2022-06-03 18:07:06 -0300

    StylesSuccessSimple

--- client/src/components/ResponePurchase/Succes.jsx
+++ client/src/components/ResponePurchase/Succes.jsx
@@ -4,6 +4,9 @@ import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
 import { putPurchase } from "../../redux/actions/actions";
 
+//styles
+import Style from "./Success.module.css"
+
 export function Succes() {
     const dispatch = useDispatch();
     const search = useLocation().search;
@@ -24,15 +27,10 @@ useEffect(() => {
 }, []);
 
     return (
-    
-        
-
-        <div>
-      { status==='pending'? <h1> Estamos Esperando Tu pago</h1>:status==='rejected'?<h1> Compra cancelada, intenta nuevamente </h1>: <h1>Muchas Gracis por su compra</h1>
-         }
-
- <Link to= '/'> <button> Inicio</button></Link>
-
+        <div className={Style.hache1}>
+        {status==='pending'? <h1> Estamos esperando tu pago</h1>:status==='rejected'?<h1> Compra cancelada, intenta nuevamente </h1>: <h1>Muchas gracias por tu compra</h1>
+        }
+        <Link to= '/'> <button className={Style.buttom}>Inicio</button></Link>
         </div>
     )
 }
