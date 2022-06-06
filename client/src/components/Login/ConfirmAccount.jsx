d713add253dc32477fd0e72c274abf7238275542
Martin Nicolas Russo <russomartin2@gmail.com>
2022-06-06 19:11:34 -0300

    EstilosACC

--- client/src/components/Login/ConfirmAccount.jsx
+++ client/src/components/Login/ConfirmAccount.jsx
@@ -24,11 +24,17 @@ function ConfirmAccount() {
     }
   return (
     <div className={Style.backg}>
+      <div className={Style.textContainer}>
+        <div className={Style.background}>
+      <h1>Bienvenido!</h1>
     <h2 className={Style.spacing}>Tu cuenta ha sido verificada correctamente!</h2>
     <h3 className={Style.p}> Ahora puedes disfrutar todas las funcionalidades de nuestra pagina</h3>
 
 
     <Link to='/'><button className={Style.buttom}>Home</button></Link>
+        </div>
+
+      </div>
 
 
     </div>
