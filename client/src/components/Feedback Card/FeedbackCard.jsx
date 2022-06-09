233d694d3755511ecf46b1c0ba0798b32dd640ec
RomaGarcia <roarga10@gmail.com>
2022-06-09 17:11:27 -0300

    review of product done

--- client/src/components/Feedback Card/FeedbackCard.jsx
+++ client/src/components/Feedback Card/FeedbackCard.jsx
@@ -2,20 +2,17 @@ import React,{useState} from 'react';
 import style from './FeedbackCard.module.css';
 import Rating from '@mui/material/Rating';
 
-export const FeedbackCard = ({name,email, comment,title,ranking,sum,setSum}) => {
-  setSum(ranking+sum)
+export const FeedbackCard = ({name,email, comment,title,ranking}) => {
+
 return (
     <div className={style.review}>
-        {comment !== 'Este vino aun no tiene comentarios'
-        ?
         <div className={style.infoRev}>
-          <Rating name="read-only" value={ranking?ranking:3} readOnly />
-          <h3 className={style.h3}>{title?title.toUpperCase():'SIN TITULO'}</h3>
+          <Rating name="read-only" value={ranking} readOnly />
+          <h3 className={style.h3}>{title}</h3>
           <h4 className={style.email}>Email: {email} </h4> 
           <h4 className={style.email}> Nombre: {name}</h4> 
-          <p className={style.p}> Comentario: {comment?comment:'No Hay Comentarios Aun'}</p>
+          <p className={style.p}> Comentario: {comment}</p>
         </div>
-        : <div className={style.noRev}> <p>Este vino aun no tiene comentarios</p></div>}
     </div>
   )
 }
